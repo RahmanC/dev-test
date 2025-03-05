e73e59704f195d31091a95bfb163ce320da7a1f1
Abdulrahman <olalekanrahman06@gmail.com>
2025-03-05 13:04:14 +0100

    fix:input value

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -17,7 +17,7 @@ export const Input: FC<IInput> = ({
       <input
         type={type}
         placeholder={placeholder}
-        value={value}
+        defaultValue={value}
         className="w-full px-3 py-2.5  rounded-lg shadow-[0px_1px_2px_0px_#1212170D,0px_0px_0px_1px_#D6D6D6B2_inset] focus:outline-none  
         focus:shadow-[0px_0px_0px_3px_#ECE9FE,0px_1px_2px_0px_#1212170D,0px_0px_0px_1px_#A48AFB_inset] focus:border-transparent
         placeholder:text-[#A3A3A3] text-sm font-normal hover:shadow-[0px_0px_0px_3px_#D6D6D6B2_inset,0px_1px_2px_0px_#1212170D,0px_0px_0px_1px_#A48AFB_inset] text-[#292929]"
